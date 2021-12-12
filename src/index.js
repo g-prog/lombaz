cc89f8f41ef4b11cbe72d2ffac9dfaa3a044fbdf
g-prog <alaobukky2@gmail.com>
2021-12-12 05:23:29 -0800

    added a webpack bundler, installed firebase and set up the firebase backend

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import {initializeApp} from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAi0AnFVjmw9rtWE8AsU7QJ9DKdDbTg4bc",
+    authDomain: "lombaz-f3351.firebaseapp.com",
+    projectId: "lombaz-f3351",
+    storageBucket: "lombaz-f3351.appspot.com",
+    messagingSenderId: "163673553412",
+    appId: "1:163673553412:web:1a3b5a652be258956e1351"
+  }; 
+
+initializeApp(firebaseConfig)
