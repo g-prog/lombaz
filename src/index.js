0e7bac18d01140c80643d5d584619cbf26bbd836
g-prog <alaobukky2@gmail.com>
2021-12-12 13:52:13 -0800

    finished up the task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {initializeApp} from 'firebase/app'
+import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyAi0AnFVjmw9rtWE8AsU7QJ9DKdDbTg4bc",
@@ -8,5 +9,56 @@ const firebaseConfig = {
     messagingSenderId: "163673553412",
     appId: "1:163673553412:web:1a3b5a652be258956e1351"
   }; 
-
+//init firebase app
 initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+
+
+
+//collection ref
+const colRef = collection(db, 'books')
+
+
+
+
+
+
+//get collection data
+getDocs(colRef)
+  .then((snapshot) =>{
+    console.log(snapshot.docs)
+    let books = []
+    snapshot.docs.forEach((doc)=> {
+      books.push({ ...doc.data(), id: doc.id })
+
+    })
+    console.log(books)
+  })
+  .catch( err => {
+    console.log(err.message)
+  })
+
+  // adding documents to the firestore backend
+
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  addDoc(colRef, {
+    name: addBookForm.name.value,
+    description:addBookForm.description.value,
+    phone :addBookForm.phone.value,
+    tracking:addBookForm.tracking.value,
+    sendername:addBookForm.sname.value,
+    receivername:addBookForm.rname.value,
+    senderaddress:addBookForm.saddress.value,
+    receiveraddress:addBookForm.raddress.value
+
+  })
+  .then(() => {
+    addBookForm.reset()
+  })
+})
